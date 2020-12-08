ef83ad181abc687bc22a1a59f74fc9d5c852354b
plondon <philip.n.london@gmail.com>
2020-12-08 14:07:56 +0100

    Add login mutation

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,26 +1,80 @@
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import { User } from "../entities/User";
 import { MyContext } from "../types";
-import argon2 from 'argon2'
+import argon2 from "argon2";
 
 @InputType()
 class UsernamePasswordInput {
   @Field()
-  username: string
+  username: string;
   @Field()
-  password: string
+  password: string;
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
 }
 
 @Resolver()
 export class UserResolver {
   @Mutation(() => User)
   async register(
-    @Arg('options') options: UsernamePasswordInput,
+    @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
   ): Promise<User> {
-    const hashedPw = await argon2.hash(options.password)
-    const user = em.create(User, { username: options.username, password: hashedPw  })
-    await em.persistAndFlush(user)
-    return user
+    const hashedPw = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPw,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username });
+
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "User does not exist." }],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password)
+
+    if (!valid) {
+      return {
+        errors: [{ field: 'password', message: "Incorrect password." }]
+      }
+    }
+
+    return {
+      user
+    };
   }
 }
