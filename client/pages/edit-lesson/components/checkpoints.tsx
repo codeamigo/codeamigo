888108ea76eef15ada65dfd70b5a57cc07bfb46e
plondon <philip.n.london@gmail.com>
2020-12-18 13:39:12 +0100

    Clean up checkpoints

--- client/pages/edit-lesson/components/checkpoints.tsx
+++ client/pages/edit-lesson/components/checkpoints.tsx
@@ -16,7 +16,6 @@ const Checkpoints: React.FC<Props> = ({ step }: Props) => {
   );
 
   useEffect(() => {
-    console.log(data);
     setCheckpoints(data?.step?.checkpoints || []);
   }, [data?.step?.checkpoints]);
 
@@ -37,29 +36,38 @@ const Checkpoints: React.FC<Props> = ({ step }: Props) => {
   };
 
   return (
-    <>
+    <div className='px-4 py-5'>
       {checkpoints.length
         ? checkpoints.map((checkpoint, i) => {
             return (
-              <div key={checkpoint.id}>
-                <h3>Checkpoint {i + 1}</h3>
+              <div key={checkpoint.id} className="mb-6">
+                <h3>
+                  <span>Checkpoint {i + 1} </span>
+                  {i === checkpoints.length - 1 ? (
+                    <button
+                      onClick={() => deleteCheckpoint(checkpoint.id)}
+                      className="inline-flex justify-center py-1 px-2 border border-transparent shadow-xs text-xs font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-red-500 disabled:opacity-50"
+                      type="button"
+                    >
+                      Delete
+                    </button>
+                  ) : null}
+                </h3>
                 <div>Markdown editor</div>
-
-                {i === checkpoints.length - 1 ? (
-                  <button onClick={() => deleteCheckpoint(checkpoint.id)}>
-                    Delete Checkpoint
-                  </button>
-                ) : null}
               </div>
             );
           })
         : null}
-      <div>
-        <button type="button" onClick={createCheckpoint}>
+      <div className="mt-4 my-8">
+        <button
+          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
+          onClick={createCheckpoint}
+          type="button"
+        >
           Add Checkpoint
         </button>
       </div>
-    </>
+    </div>
   );
 };
 
