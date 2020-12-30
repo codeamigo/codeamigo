73e4ff09b16a0f89907ca6ff45b2f85c061018a7
plondon <philip.n.london@gmail.com>
2020-12-30 14:47:52 +0100

    Move preview to public folder

--- client/preview/server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var port = 1234;
-
-app.use(express.static('public'));
-
-app.listen(port, function () {
-  console.log('Example app listening at http://localhost:' + port);
-});
