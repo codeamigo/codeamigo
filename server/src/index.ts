5f330429d10a94ba03d2b4bafd9e6934d4ab5731
plondon <philip.n.london@gmail.com>
2020-12-31 17:24:25 +0100

    Trust proxy fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,7 +37,7 @@ const main = async () => {
   await conn.runMigrations();
 
   const app = express();
-  app.set("proxy", 1);
+  app.set("trust proxy", 1);
   app.use(
     cors({
       credentials: true,
