ef95d3be4b5bdd7e89d31793ec4d40b711ec5d54
plondon <philip.n.london@gmail.com>
2021-01-06 13:58:48 +0100

    Fix next step

--- client/widgets/Lesson/Checkpoints/index.tsx
+++ client/widgets/Lesson/Checkpoints/index.tsx
@@ -196,7 +196,8 @@ const Checkpoints: React.FC<Props> = ({ isEditting, nextStep, step }) => {
         </div>
       )}
       {!isEditting &&
-        checkpoints.some((checkpoint) => !!checkpoint.isCompleted) && (
+        (checkpoints.some((checkpoint) => !!checkpoint.isCompleted) ||
+          !checkpoints.length) && (
           <div className="mt-4">
             <button
               className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50"
