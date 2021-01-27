e6c6ddc52c3fd5da85466ed956281c42c482438c
plondon <philip.n.london@gmail.com>
2021-01-27 12:57:40 +0100

    Lessons empty state

--- client/widgets/SessionsList/index.tsx
+++ client/widgets/SessionsList/index.tsx
@@ -9,9 +9,11 @@ import SessionItem from './SessionItem';
 const SessionsList: React.FC<Props> = () => {
   const router = useRouter();
 
-  const { data } = useSessionsQuery({
+  const { data, loading } = useSessionsQuery({
     fetchPolicy: 'cache-and-network',
   });
+
+  if (loading) return null;
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
@@ -21,7 +23,7 @@ const SessionsList: React.FC<Props> = () => {
         })
       ) : (
         <div
-          className="flex flex-col justify-center items-center h-36 w-36 bg-gray-100 text-gray-500 rounded-md cursor-pointer hover:text-blue-600 transform hover:scale-105 transition-colors transition-transform duration-300"
+          className="flex flex-col justify-center items-center h-36 w-36 bg-gray-100 text-gray-600 rounded-md cursor-pointer hover:text-blue-600 transform hover:scale-105 transition-colors transition-transform duration-300"
           onClick={() => router.push('/')}
         >
           <Icon className="text-xl" name="plus-squared" />
--- client/widgets/UserLessonsList/index.tsx
+++ client/widgets/UserLessonsList/index.tsx
@@ -1,17 +1,35 @@
 import React from 'react';
 
+import { modalVar } from '👨‍💻apollo/cache/modal';
+import Icon from '👨‍💻components/Icon';
 import { useUserLessonsQuery } from '👨‍💻generated/graphql';
 
 import LessonItem from './LessonItem';
 
 const UserLessonsList: React.FC<Props> = () => {
-  const { data } = useUserLessonsQuery({ fetchPolicy: 'cache-and-network' });
+  const { data, loading } = useUserLessonsQuery({
+    fetchPolicy: 'cache-and-network',
+  });
+
+  if (loading) return null;
 
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-      {data?.userLessons?.map((lesson) => {
-        return <LessonItem key={lesson.id} lesson={lesson} />;
-      })}
+      {data?.userLessons.length ? (
+        data?.userLessons?.map((lesson) => {
+          return <LessonItem key={lesson.id} lesson={lesson} />;
+        })
+      ) : (
+        <div
+          className="flex flex-col justify-center items-center h-36 w-36 bg-gray-100 text-gray-600 rounded-md cursor-pointer hover:text-blue-600 transform hover:scale-105 transition-colors transition-transform duration-300"
+          onClick={() =>
+            modalVar({ callback: () => null, name: 'createLesson' })
+          }
+        >
+          <Icon className="text-xl" name="plus-squared" />
+          <p className="text-sm font-bold">New Lesson</p>
+        </div>
+      )}
     </div>
   );
 };
