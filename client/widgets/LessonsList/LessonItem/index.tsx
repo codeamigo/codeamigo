7ccd22e12af79360505f69063dbece7c07dbdf61
plondon <philip.n.london@gmail.com>
2021-03-13 13:15:27 -0500

    Add to lesson list

--- client/widgets/LessonsList/LessonItem/index.tsx
+++ client/widgets/LessonsList/LessonItem/index.tsx
@@ -26,32 +26,40 @@ const LessonItem: React.FC<Props> = ({ lesson }) => {
   };
 
   return (
-    <div
-      className="p-3 rounded-lg border-bg-nav-offset border-2"
-      key={lesson.id}
-    >
-      <a
-        className="text-md text-accent font-semibold hover:underline"
-        href="/"
-        onClick={(e) => handleClick(e, lesson.id)}
-      >
-        {lesson.title}
-      </a>
-      <h3 className="text-xs text-text-primary">By: {lesson.owner.username}</h3>
-      <div className="flex justify-between mt-4 text-xs text-text-primary">
-        <div
-          aria-label={`${lesson.students?.length} Students`}
-          className="hint--top hint--no-animate"
+    <div className="rounded-lg border-bg-nav-offset border-4 overflow-hidden">
+      <div
+        className="h-48 bg-center bg-cover bg-no-repeat border-b-2 border-bg-nav-offset"
+        style={{ backgroundImage: `url(${lesson.thumbnail}` }}
+      />
+      <div className="p-3" key={lesson.id}>
+        <a
+          className="text-md text-accent font-semibold hover:underline"
+          href="/"
+          onClick={(e) => handleClick(e, lesson.id)}
         >
-          <div className="flex">
-            <Icon className="text-text-primary mr-1 cursor-auto" name="users" />{' '}
-            <div className="text-text-primary">{lesson.students?.length}</div>
+          {lesson.title}
+        </a>
+        <h3 className="text-xs text-text-primary">
+          By: {lesson.owner.username}
+        </h3>
+        <div className="flex justify-between mt-4 text-xs text-text-primary">
+          <div
+            aria-label={`${lesson.students?.length} Students`}
+            className="hint--top hint--no-animate"
+          >
+            <div className="flex">
+              <Icon
+                className="text-text-primary mr-1 cursor-auto"
+                name="users"
+              />{' '}
+              <div className="text-text-primary">{lesson.students?.length}</div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex justify-between items-center mt-2 text-xs text-text-primary">
-        <LanguageBar steps={lesson.steps} />
-        <div>{new Date(parseInt(lesson.createdAt)).toDateString()}</div>
+        <div className="flex justify-between items-center mt-2 text-xs text-text-primary">
+          <LanguageBar steps={lesson.steps} />
+          <div>{new Date(parseInt(lesson.createdAt)).toDateString()}</div>
+        </div>
       </div>
     </div>
   );
