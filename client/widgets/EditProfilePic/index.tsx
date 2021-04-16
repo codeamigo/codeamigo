09c00e4155270f313c52d1a966ad3a1abbae7be5
plondon <philip.n.london@gmail.com>
2021-04-16 18:26:35 -0400

    animate pulse pending pic

--- client/widgets/EditProfilePic/index.tsx
+++ client/widgets/EditProfilePic/index.tsx
@@ -1,30 +1,11 @@
 import React from 'react';
 
 import Icon from '👨‍💻components/Icon';
-import { Logo } from '👨‍💻components/Logos';
-import {
-  useProfileColorSchemeQuery,
-  useUpdateProfileColorSchemeMutation,
-} from '👨‍💻generated/graphql';
+import { useUpdateProfileColorSchemeMutation } from '👨‍💻generated/graphql';
 import ProfileLogo from '👨‍💻widgets/ProfileLogo';
 
 const EditProfilePic: React.FC<Props> = () => {
   const [updateProfileColorScheme] = useUpdateProfileColorSchemeMutation();
-  const { data } = useProfileColorSchemeQuery({
-    fetchPolicy: 'cache-and-network',
-  });
-
-  if (!data?.profileColorScheme)
-    return (
-      <div className="h-40 w-40 bg-bg-nav mb-4 rounded-full relative">
-        <Logo />
-      </div>
-    );
-
-  const [chin, ear, face, eye] = data?.profileColorScheme.split('-');
-
-  if (!data?.profileColorScheme)
-    return <div className="h-40 w-40 rounded-full" />;
 
   return (
     <div className="h-40 w-40 bg-bg-nav border border-bg-nav-offset mb-4 rounded-full relative">
