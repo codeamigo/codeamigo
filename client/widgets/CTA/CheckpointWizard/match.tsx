67c8b91cbab3cc5a2e74dafc1d44908eba76b541
plondon <philip.n.london@gmail.com>
2021-10-01 12:41:39 -0400

    Cleanup go back

--- client/widgets/CTA/CheckpointWizard/match.tsx
+++ client/widgets/CTA/CheckpointWizard/match.tsx
@@ -19,32 +19,25 @@ const Match: React.FC<Props> = ({ selectFile, setWizardStep, step }) => {
 
   return (
     <div className="text-text-primary">
-      <div className="flex mb-4">
-        <Icon
-          className="text-md"
-          name="left-bold"
-          onClick={() => setWizardStep('select')}
-          role="button"
-        />
-      </div>
       <div className="flex">
         <Icon className="text-lg block mr-2" name="regexicon" />
         <h3 className="text-lg font-bold">Match</h3>
       </div>
       <Formik
-        initialValues={{ regex: '' }}
+        initialValues={{
+          file: step.codeModules?.find(({ isEntry }) => !!isEntry)?.name,
+          regex: '',
+        }}
         onSubmit={() => console.log('todo')}
       >
         {({ values }) => (
           <>
             <Form>
-              <div className="mt-2 mb-1">
-                In which file should the regex be checked?
-              </div>
+              <div className="mt-2 mb-1">Select a file</div>
               <Field
                 as="select"
                 className="text-black rounded-lg text-sm w-full"
-                name="color"
+                name="file"
                 onChangeCapture={handleFileChange}
               >
                 {step?.codeModules
@@ -54,8 +47,7 @@ const Match: React.FC<Props> = ({ selectFile, setWizardStep, step }) => {
                   })}
               </Field>
               <div className="mt-3 mb-1">
-                Enter a regular expression below to match your student's desired
-                input.
+                Enter a regular expression to search
               </div>
               <InputField
                 className="text-black"
@@ -65,7 +57,13 @@ const Match: React.FC<Props> = ({ selectFile, setWizardStep, step }) => {
                 type="text"
               />
               <div className="text-xs mt-1">Regex: /{values.regex}/g</div>
-              <div className="flex justify-end mt-2">
+              <div className="flex justify-between items-center mt-2">
+                <Icon
+                  className="text-md mr-4"
+                  name="left-bold"
+                  onClick={() => setWizardStep('select')}
+                  role="button"
+                />
                 <Button type="submit">Submit</Button>
               </div>
             </Form>
