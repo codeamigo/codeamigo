5b9edd7fd4152e0a81935389106ee338ca879aa4
plondon <philip.n.london@gmail.com>
2021-10-28 08:39:58 -0400

    Amend footer about 1

--- client/layouts/MainLayout/Footer/index.tsx
+++ client/layouts/MainLayout/Footer/index.tsx
@@ -4,7 +4,9 @@ const Footer: React.FC<Props> = () => {
   return (
     <div className="bg-bg-nav text-text-primary">
       <div className="py-3 px-2 sm:px-6 lg:px-8 mx-auto w-full max-w-7xl">
-        <div className="text-lg">Byte-sized interactive coding tutorials</div>
+        <div className="text-lg font-bold">
+          Byte-sized interactive coding tutorials
+        </div>
         <div className="flex gap-2 mt-3 text-xs">
           <div
             aria-label="No landing page.&#10;No signup required.&#10;Nothing to buy.&#10;Codeamigo.dev allows anyone to take&#10;free interactive coding tutorials.&#10;Built by the community."
