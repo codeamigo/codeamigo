f722edd0d51a38ba9c44628b45dd6af7cbb451c9
plondon <philip.n.london@gmail.com>
2021-10-31 16:22:59 -0400

    Try new cache-control headers

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        headers: [{ key: 'Cache-Control', value: 'stale-while-revalidate' }],
+        source: '/',
+      },
+    ];
+  },
   webpack: (config, { buildId, defaultLoaders, dev, isServer, webpack }) => {
     return config;
   },
