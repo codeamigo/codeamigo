063346c8a1bdf767b373aa07456ac3ebe0724313
plondon <philip.n.london@gmail.com>
2021-10-31 16:41:38 -0400

    Try new cache-control headers 4

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        headers: [
-          {
-            key: 'cache-control',
-            value: 's-maxage=1, stale-while-revalidate=59',
-          },
-        ],
-        source: '/',
-      },
-    ];
-  },
   webpack: (config, { buildId, defaultLoaders, dev, isServer, webpack }) => {
     return config;
   },
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -19,4 +19,15 @@ const Home = () => {
   );
 };
 
+export async function getServerSideProps({ req, res }) {
+  res.setHeader(
+    'Cache-Control',
+    'public, s-maxage=10, stale-while-revalidate=59'
+  );
+
+  return {
+    props: {},
+  };
+}
+
 export default withApollo({ ssr: true })(Home);
