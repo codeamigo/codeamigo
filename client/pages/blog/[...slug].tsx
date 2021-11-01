d5b67c22c34dc7f94fe8f351bf2bfe19b3020d3b
plondon <philip.n.london@gmail.com>
2021-11-01 08:58:32 -0400

    Redirect to blog

---
+++ client/pages/blog/[...slug].tsx
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
+const BlogPost: React.FC<Props> = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.log(router.query.slug);
+    if (router.query?.slug?.length) {
+      window.location.replace(
+        `https://docs.codeamigo.dev/blog/${router.query.slug[0]}`
+      );
+    }
+  }, [router.query.slug]);
+
+  return <div></div>;
+};
+
+type Props = {};
+
+export default BlogPost;
