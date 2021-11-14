2a8ef9952cd6c90fbe2ca492961cb505683be881
plondon <philip.n.london@gmail.com>
2021-11-14 13:03:05 -0500

    Add zigpoll

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import Dependencies from '👨‍💻widgets/HomepageFilters/Dependencies';
 import LanguagesAndTemplates from '👨‍💻widgets/HomepageFilters/LanguagesAndTemplates';
 import Levels from '👨‍💻widgets/HomepageFilters/Levels';
@@ -6,6 +8,22 @@ import LessonsList from '👨‍💻widgets/LessonsList';
 const Home = () => {
   return (
     <div className="flex flex-col sm:flex-row sm:space-x-8">
+      <Head>
+        <script type="application/javascript">
+          {`;(function () {
+    window.Zigpoll = {
+      accountId: '61903b87a693e36a9327bd62'
+    };
+
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.charset = "utf-8";
+    script.src = "//cdn.zigpoll.com/static/js/main.js";
+
+    document.head.appendChild(script);
+  }());`}
+        </script>
+      </Head>
       <div className="sm:sticky sm:top-4 mb-4 w-full sm:w-5/12 md:w-1/4 h-full">
         <LanguagesAndTemplates />
         <Dependencies />
@@ -14,6 +32,7 @@ const Home = () => {
       <div className="w-full sm:w-7/12 md:w-3/4">
         <LessonsList />
       </div>
+      <div id="zigpoll-embed-target"></div>
     </div>
   );
 };
