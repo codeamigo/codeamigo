4b78bdff973506c14fc1a75e8e978a53478282b7
plondon <philip.n.london@gmail.com>
2021-11-14 15:24:56 -0500

    Start on nextjs12

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  experimental: {
+    concurrentFeatures: true,
+    serverComponents: true,
+  },
   webpack: (config, { buildId, defaultLoaders, dev, isServer, webpack }) => {
     return config;
   },
