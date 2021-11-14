da1c0fe35e51bffe55c3dae7629680b384d5059d
plondon <philip.n.london@gmail.com>
2021-11-14 18:40:59 -0500

    Remove experimental features

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  experimental: {
-    concurrentFeatures: true,
-    serverComponents: true,
-  },
   webpack: (config, { buildId, defaultLoaders, dev, isServer, webpack }) => {
     return config;
   },
