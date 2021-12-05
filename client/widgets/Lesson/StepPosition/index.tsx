fb1d768c3a811bb59653d8bae94d97660cc2cfc3
plondon <philip.n.london@gmail.com>
2021-12-05 11:34:34 -0500

    Steps on mobile should just be current / length

--- client/widgets/Lesson/StepPosition/index.tsx
+++ client/widgets/Lesson/StepPosition/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const StepPosition: React.FC<Props> = ({ currentStepNum, totalStepsNum }) => {
   return (
     <div className="text-sm font-bold text-text-primary">
-      Step {currentStepNum}/{totalStepsNum}
+      <span className="hidden md:inline-block">Step</span> {currentStepNum}/
+      {totalStepsNum}
     </div>
   );
 };
