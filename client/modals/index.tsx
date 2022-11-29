9af27b1b865ba5164409fcaf682bfeb48fe8b668
plondon <philip.n.london@gmail.com>
2022-11-29 10:45:26 -0500

    Remove w full on modals

--- client/modals/index.tsx
+++ client/modals/index.tsx
@@ -57,7 +57,7 @@ const Modals: React.FC<Props> = () => {
         >
           <div
             aria-hidden="true"
-            className="fixed inset-0 bg-bg-nav opacity-50 transition-opacity"
+            className="fixed inset-0 opacity-50 transition-opacity bg-bg-nav"
             onClick={() => modalVar(InitialModalState)}
           >
             <div className="absolute inset-0 opacity-75"></div>
@@ -82,11 +82,11 @@ const Modals: React.FC<Props> = () => {
         <div
           aria-labelledby="modal-headline"
           aria-modal="true"
-          className="inline-block w-full sm:w-auto text-left align-bottom sm:align-middle bg-bg-primary rounded-lg shadow-xl transition-all transform"
+          className="inline-block text-left align-bottom sm:align-middle rounded-lg shadow-xl transition-all transform bg-bg-primary"
           role="dialog"
         >
           <div
-            className="flex absolute -top-3 -right-3 justify-center items-center w-10 h-10 bg-bg-primary rounded-full"
+            className="flex absolute -top-3 -right-3 justify-center items-center w-10 h-10 rounded-full bg-bg-primary"
             onClick={() => modalVar(InitialModalState)}
           >
             <Icon
