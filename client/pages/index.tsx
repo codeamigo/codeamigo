11f66a4e23e1450c0d71af61c90e538e9a3f2862
plondon <philip.n.london@gmail.com>
2023-03-23 10:17:28 -0400

    code model setup for monaco editor

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,12 +7,17 @@ import {
   useActiveCode,
   useSandpack,
 } from '@codesandbox/sandpack-react';
-import Editor, { Monaco } from '@monaco-editor/react';
+import Editor from '@monaco-editor/react';
 import { debounce } from 'debounce';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
 
+import { getLanguage } from '👨‍💻widgets/Lesson/Editor/utils';
 import PrevNext from '👨‍💻widgets/PrevNext';
+
+const URN = 'urn:';
 
 const steps = [
   {
@@ -33,15 +38,74 @@ const steps = [
         code: 'body {\n  font-family: sans-serif;\n  -webkit-font-smoothing: auto;\n  -moz-font-smoothing: auto;\n  -moz-osx-font-smoothing: grayscale;\n  font-smoothing: auto;\n  text-rendering: optimizeLegibility;\n  font-smooth: always;\n  -webkit-tap-highlight-color: transparent;\n  -webkit-touch-callout: none;\n}\n\nh1 {\n  font-size: 1.5rem;\n}',
       },
     },
+    instructions: 'Start typing to get suggestions for foods you like to eat.',
     start: 'I like to eat ',
+  },
+  {
+    files: {
+      '/App.js': {
+        code: 'export default function App() {\n // Create an element that can be used to display a box \n return \n}\n',
+      },
+      '/index.js': {
+        code: 'import React, { StrictMode } from "react";\nimport { createRoot } from "react-dom/client";\nimport "./styles.css";\n\nimport App from "./App";\n\nconst root = createRoot(document.getElementById("root"));\nroot.render(\n  <StrictMode>\n    <App />\n  </StrictMode>\n);',
+      },
+      '/package.json': {
+        code: '{\n  "dependencies": {\n    "react": "^18.0.0",\n    "react-dom": "^18.0.0",\n    "react-scripts": "^4.0.0"\n  },\n  "main": "/index.js",\n  "devDependencies": {}\n}',
+      },
+      '/public/index.html': {
+        code: '<!DOCTYPE html>\n<html lang="en">\n  <head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <title>Document</title>\n  </head>\n  <body>\n    <div id="root"></div>\n  </body>\n</html>',
+      },
+      '/styles.css': {
+        code: 'body {\n  font-family: sans-serif;\n  -webkit-font-smoothing: auto;\n  -moz-font-smoothing: auto;\n  -moz-osx-font-smoothing: grayscale;\n  font-smoothing: auto;\n  text-rendering: optimizeLegibility;\n  font-smooth: always;\n  -webkit-tap-highlight-color: transparent;\n  -webkit-touch-callout: none;\n}\n\nh1 {\n  font-size: 1.5rem;\n}',
+      },
+    },
+    instructions: 'Create an element that can be used to display a box',
+    start: ' return ',
   },
 ];
 
-function MonacoEditor() {
+function usePrevious(value: any) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+function MonacoEditor({
+  currentStep,
+  files,
+}: {
+  currentStep: number;
+  files: any;
+}) {
   const { code, updateCode } = useActiveCode();
   const { sandpack } = useSandpack();
-  const [editor, setEditor] = useState<any>();
+  const { activeFile } = sandpack;
   const [completions, setCompletions] = useState<any>([]);
+  const editorRef = useRef<any>();
+  const monacoRef = useRef<any>();
+
+  useEffect(() => {
+    if (!monacoRef.current) return;
+    if (!editorRef.current) return;
+    if (!activeFile) return;
+    const model = monacoRef.current.editor.getModel(
+      monacoRef.current.Uri.parse(`${URN}${activeFile}`)
+    );
+    if (model) editorRef.current.setModel(model);
+  }, [activeFile, monacoRef.current, editorRef.current]);
+
+  useEffect(() => {
+    if (monacoRef.current) {
+      monacoRef.current.editor
+        .getModels()
+        .forEach((model: any) => model.dispose());
+      setupModels();
+    }
+  }, [currentStep]);
 
   const updatePrompt = async (value: string | undefined, ev: any) => {
     if (!value || !ev) return;
@@ -49,41 +113,134 @@ function MonacoEditor() {
     const line = lines[ev.changes[0].range.startLineNumber - 1];
     const changePos = ev.changes[0].range.startColumn;
     const insert =
-      line.substring(0, changePos) + '[insert]' + line.substring(changePos);
+      line.substring(0, changePos) + ' [insert] ' + line.substring(changePos);
     lines[ev.changes[0].range.startLineNumber - 1] = insert;
-    const prompt = lines.join('\n').split('[insert]')[0];
-    const suffix = lines.join('\n').split('[insert]')[1];
+    const prompt = lines.join('\n').split(' [insert] ')[0];
+    const suffix = lines.join('\n').split(' [insert] ')[1];
 
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/completions`,
-        {
-          body: JSON.stringify({
-            prompt,
-            suffix,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-        }
-      );
+    // try {
+    //   const response = await fetch(
+    //     `${process.env.NEXT_PUBLIC_API_URL}/completions`,
+    //     {
+    //       body: JSON.stringify({
+    //         prompt,
+    //         suffix,
+    //       }),
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       method: 'POST',
+    //     }
+    //   );
 
-      const completions = await response.json();
-      const uniqueCompletions = [
-        ...new Set(completions.map((item: any) => item.text)),
-      ];
+    //   const completions = await response.json();
+    //   const uniqueCompletions = [
+    //     ...new Set(completions.map((item: any) => item.text)),
+    //   ];
 
-      setCompletions(uniqueCompletions);
-    } catch (error) {
-      console.log(error);
-    }
+    //   setCompletions(uniqueCompletions);
+    // } catch (error) {
+    //   console.log(error);
+    // }
   };
 
   const debouncedUpdatePrompt = useMemo(() => debounce(updatePrompt, 100), []);
 
+  const setupModels = () => {
+    if (!monacoRef.current) return;
+    if (!editorRef.current) return;
+
+    Object.keys(files).forEach((mod) => {
+      const model = monacoRef.current.editor.getModel(
+        monacoRef.current.Uri.parse(`${URN}${mod}`)
+      );
+      if (model) return;
+      monacoRef.current.editor.createModel(
+        files[mod].code,
+        getLanguage(mod || ''),
+        monacoRef.current.Uri.parse(`${URN}${mod}`)
+      );
+    });
+    const model = monacoRef.current.editor.getModel(
+      monacoRef.current.Uri.parse(`${URN}${activeFile}`)
+    );
+    model?.updateOptions({ tabSize: 2 });
+    editorRef.current.setModel(model);
+  };
+
+  const setupCompilerOptions = () => {
+    const jsxFactory = 'React.createElement';
+    const reactNamespace = 'React';
+    const hasNativeTypescript = false;
+
+    monacoRef.current.languages.typescript.javascriptDefaults.setEagerModelSync(
+      true
+    );
+
+    const tsConfigFile = Object.keys(files).find(
+      (module) => module === '/tsconfig.json'
+    );
+    const tsConfig = tsConfigFile
+      ? JSON.parse(files[tsConfigFile].code || '')
+      : undefined;
+
+    // https://github.com/codesandbox/codesandbox-client/blob/master/packages/app/src/embed/components/Content/Monaco/index.js
+    monacoRef.current.languages.typescript.typescriptDefaults.setCompilerOptions(
+      {
+        allowJs: true,
+        allowNonTsExtensions: !hasNativeTypescript,
+        experimentalDecorators: true,
+        jsx: tsConfig?.compilerOptions
+          ? tsConfig?.compilerOptions.jsx
+          : monacoRef.current.languages.typescript.JsxEmit.React,
+        jsxFactory,
+        module: hasNativeTypescript
+          ? monacoRef.current.languages.typescript.ModuleKind.ES2015
+          : monacoRef.current.languages.typescript.ModuleKind.System,
+        moduleResolution:
+          monacoRef.current.languages.typescript.ModuleResolutionKind.NodeJs,
+        // forceConsistentCasingInFileNames:
+        //   hasNativeTypescript && existingConfig.forceConsistentCasingInFileNames,
+        // noImplicitReturns:
+        //   hasNativeTypescript && existingConfig.noImplicitReturns,
+        // noImplicitThis: hasNativeTypescript && existingConfig.noImplicitThis,
+        // noImplicitAny: hasNativeTypescript && existingConfig.noImplicitAny,
+        // strictNullChecks: hasNativeTypescript && existingConfig.strictNullChecks,
+        // suppressImplicitAnyIndexErrors:
+        //   hasNativeTypescript && existingConfig.suppressImplicitAnyIndexErrors,
+        // noUnusedLocals: hasNativeTypescript && existingConfig.noUnusedLocals,
+        newLine: monacoRef.current.languages.typescript.NewLineKind.LineFeed,
+
+        noEmit: true,
+
+        reactNamespace,
+
+        target: monacoRef.current.languages.typescript.ScriptTarget.ES2016,
+
+        typeRoots: [`node_modules/@types`],
+      }
+    );
+  };
+
+  const handleMount = (editor: any, monaco: any) => {
+    if (editorRef.current) return;
+    if (monacoRef.current) return;
+    editorRef.current = editor;
+    monacoRef.current = monaco;
+
+    setupModels();
+    setupCompilerOptions();
+
+    const match = editor
+      .getModel()
+      .findMatches(steps[currentStep].start, true, false, false, null, true)[0];
+    if (!match) return;
+    editor.setPosition(match.range.getEndPosition());
+    editor.focus();
+  };
+
   return (
-    <SandpackStack className="relative" style={{ height: '100vh', margin: 0 }}>
+    <SandpackStack className="relative" style={{ height: '100%', margin: 0 }}>
       <FileTabs />
       <Editor
         defaultValue={code}
@@ -95,19 +252,13 @@ function MonacoEditor() {
           updateCode(value || '');
           debouncedUpdatePrompt(value, ev);
         }}
-        onMount={(editor) => {
-          setEditor(editor);
-          const match = editor
-            .getModel()
-            .findMatches(steps[0].start, true, false, false, null, true)[0];
-          editor.setPosition(match.range.getEndPosition());
-          editor.focus();
-        }}
+        onMount={handleMount}
         options={{
           fontSize: 16,
           minimap: {
             enabled: false,
           },
+          quickSuggestions: false,
           scrollBeyondLastLine: false,
         }}
         theme="vs-dark"
@@ -132,28 +283,28 @@ function MonacoEditor() {
             >
               {completions.map((completion: string) => (
                 <motion.div
-                  className="relative cursor-pointer rounded-md bg-white p-2 text-black hover:bg-gray-200"
+                  className="relative cursor-pointer rounded-md bg-white p-2 font-bold text-black hover:bg-gray-200"
                   key={completion}
                   onClick={() => {
-                    editor.executeEdits('my-source', [
+                    editorRef.current.executeEdits('my-source', [
                       {
                         forceMoveMarkers: true,
                         identifier: { major: 1, minor: 1 },
                         // @ts-ignore
                         range: new monaco.Range(
-                          editor.getPosition().lineNumber,
-                          editor.getPosition().column,
-                          editor.getPosition().lineNumber,
-                          editor.getPosition().column
+                          editorRef.current.getPosition().lineNumber,
+                          editorRef.current.getPosition().column,
+                          editorRef.current.getPosition().lineNumber,
+                          editorRef.current.getPosition().column
                         ),
                         text: completion,
                       },
                     ]);
-                    editor.setPosition({
-                      column: editor.getPosition().column,
-                      lineNumber: editor.getPosition().lineNumber,
+                    editorRef.current.setPosition({
+                      column: editorRef.current.getPosition().column,
+                      lineNumber: editorRef.current.getPosition().lineNumber,
                     });
-                    editor.focus();
+                    editorRef.current.focus();
                     setCompletions([]);
                   }}
                   variants={{
@@ -173,24 +324,42 @@ function MonacoEditor() {
 }
 
 const Home = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center gap-3 p-5">
       <div
-        className="overflow-hidden rounded-lg border-2 border-gray-700"
+        className="overflow-hidden rounded-lg border border-neutral-700"
         style={{ height: '100%', width: '92%' }}
       >
         <SandpackProvider
-          files={steps[0].files}
+          files={steps[currentStep].files}
           template="react"
           theme={'dark'}
         >
           <SandpackLayout>
-            <MonacoEditor />
+            <SandpackStack>
+              <div className="h-72 overflow-scroll border-b border-neutral-700 bg-neutral-900">
+                <ReactMarkdown
+                  children={steps[currentStep].instructions}
+                  className="markdown-body py-2 px-3"
+                  plugins={[gfm]}
+                />
+              </div>
+              <MonacoEditor
+                currentStep={currentStep}
+                files={steps[currentStep].files}
+              />
+            </SandpackStack>
             <SandpackPreview />
           </SandpackLayout>
         </SandpackProvider>
       </div>
-      <PrevNext />
+      <PrevNext
+        currentStep={currentStep}
+        setCurrentStep={setCurrentStep}
+        steps={steps.length}
+      />
     </div>
   );
 };
--- client/widgets/PrevNext/index.tsx
+++ client/widgets/PrevNext/index.tsx
@@ -2,19 +2,37 @@ import React from 'react';
 
 import Icon from '👨‍💻components/Icon';
 
-const PrevNext: React.FC<Props> = () => {
+const PrevNext: React.FC<Props> = ({ currentStep, setCurrentStep, steps }) => {
   return (
     <div className="flex items-center gap-2">
-      <button className="rounded-md bg-gray-800 px-2 py-1 text-gray-200 hover:bg-gray-900">
-        <Icon className="" name="left" />
+      <button
+        className={`rounded-md bg-gray-800 px-2 py-1 text-gray-200 hover:bg-gray-900 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:opacity-40`}
+        disabled={currentStep === 0}
+        onClick={() => setCurrentStep(currentStep - 1)}
+      >
+        <Icon
+          className={currentStep === 0 ? 'cursor-not-allowed' : ''}
+          name="left"
+        />
       </button>
-      <button className="rounded-md bg-gray-800 px-2 py-1 text-gray-200 hover:bg-gray-900">
-        <Icon className="" name="right" />
+      <button
+        className="rounded-md bg-gray-800 px-2 py-1 text-gray-200 hover:bg-gray-900 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:opacity-40"
+        disabled={currentStep === steps - 1}
+        onClick={() => setCurrentStep(currentStep + 1)}
+      >
+        <Icon
+          className={currentStep === steps - 1 ? 'cursor-not-allowed' : ''}
+          name="right"
+        />
       </button>
     </div>
   );
 };
 
-type Props = {};
+type Props = {
+  currentStep: number;
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
+  steps: number;
+};
 
 export default PrevNext;
