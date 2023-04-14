3b263a7d5f69fbc7f2f1d1551ee3b17c4b3ad328
plondon <philip.n.london@gmail.com>
2023-04-14 19:06:19 -0400

    Intro to codeamigo

--- client/widgets/PrevNext/index.tsx
+++ client/widgets/PrevNext/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Icon from '👨‍💻components/Icon';
-
 const PrevNext: React.FC<Props> = ({
   currentStep,
   disabled,
@@ -11,26 +9,20 @@ const PrevNext: React.FC<Props> = ({
   const nextDisabled = currentStep === steps - 1 || disabled;
 
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center justify-center gap-2 bg-black py-2">
       <button
-        className={`rounded-md bg-gray-800 px-2 py-1 text-gray-200 hover:bg-gray-900 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:opacity-40`}
+        className={`rounded-md border border-neutral-800 bg-neutral-900 p-2 font-bold text-gray-200 hover:bg-neutral-800 disabled:cursor-not-allowed disabled:bg-neutral-800 disabled:opacity-40`}
         disabled={currentStep === 0}
         onClick={() => setCurrentStep(currentStep - 1)}
       >
-        <Icon
-          className={currentStep === 0 ? 'cursor-not-allowed' : ''}
-          name="left"
-        />
+        Prev
       </button>
       <button
-        className="rounded-md bg-gray-800 px-2 py-1 text-gray-200 hover:bg-gray-900 disabled:cursor-not-allowed disabled:bg-gray-600 disabled:opacity-40"
+        className="rounded-md border border-neutral-800 bg-neutral-900 p-2 font-bold text-gray-200 hover:bg-neutral-800 disabled:cursor-not-allowed disabled:bg-neutral-800 disabled:opacity-40"
         disabled={nextDisabled}
         onClick={() => setCurrentStep(currentStep + 1)}
       >
-        <Icon
-          className={currentStep === steps - 1 ? 'cursor-not-allowed' : ''}
-          name="right"
-        />
+        Next
       </button>
     </div>
   );
