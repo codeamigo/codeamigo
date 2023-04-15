a531372cb5e9cacc8fd6eca183824d8bcb3d38de
plondon <philip.n.london@gmail.com>
2023-04-15 17:09:50 -0400

    Auto next step if test pass

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -29,14 +29,13 @@ import { PostHogProvider } from 'posthog-js/react';
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== 'undefined') {
-  // @ts-ignore
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
-    // Enable debug mode in development
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug();
-    },
-  });
+  if (process.env.NODE_ENV !== 'development') {
+    // @ts-ignore
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+      api_host:
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://app.posthog.com',
+    });
+  }
 }
 
 function MyApp({ Component, pageProps, router }: AppProps) {
--- client/widgets/PrevNext/index.tsx
+++ client/widgets/PrevNext/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Button from '👨‍💻components/Button';
 import Icon from '👨‍💻components/Icon';
@@ -6,11 +6,44 @@ import Icon from '👨‍💻components/Icon';
 const PrevNext: React.FC<Props> = ({
   currentStep,
   disabled,
+  nextLoader,
   setCurrentStep,
   steps,
 }) => {
   const nextDisabled = currentStep === steps - 1 || disabled;
   const isLastStep = currentStep === steps - 1;
+  const [loaderWidth, setLoaderWidth] = useState(0);
+
+  useEffect(() => {
+    let interval: any;
+    if (nextLoader) {
+      // increase the width of the loader to 100% over 4 seconds
+      // this is the same amount of time it takes for the next button to be clicked
+      // if button is hovered over, pause the loader
+      interval = setInterval(() => {
+        if (loaderWidth < 100) {
+          setLoaderWidth((prevWidth) => prevWidth + 1);
+        }
+      }, 40);
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval);
+      }
+    };
+  }, [nextLoader]);
+
+  useEffect(() => {
+    const loader = document.getElementById('loader');
+    if (!loader) return;
+    loader.style.width = `${loaderWidth}%`;
+    const nextButton = document.getElementById('next-button');
+
+    if (loaderWidth === 100) {
+      nextButton?.click();
+    }
+  }, [loaderWidth]);
 
   return (
     <div>
@@ -34,10 +67,19 @@ const PrevNext: React.FC<Props> = ({
             Prev
           </Button>
           <Button
+            className="relative overflow-hidden"
             disabled={nextDisabled}
+            id="next-button"
             onClick={() => setCurrentStep(currentStep + 1)}
           >
-            Next
+            <span className="relative z-10">Next</span>
+            {nextLoader && (
+              // element that expands in the style of the netflix loader
+              <span
+                className="absolute left-0 z-0 h-full w-0 bg-neutral-800"
+                id="loader"
+              ></span>
+            )}
           </Button>
         </div>
       )}
@@ -48,6 +90,7 @@ const PrevNext: React.FC<Props> = ({
 type Props = {
   currentStep: number;
   disabled: boolean;
+  nextLoader: boolean;
   setCurrentStep: React.Dispatch<React.SetStateAction<number>>;
   steps: number;
 };
