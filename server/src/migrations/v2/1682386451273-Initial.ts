099a70e790527f305e2ea56a7c45c0e42062f77d
plondon <philip.n.london@gmail.com>
2023-04-24 21:34:28 -0400

    Reset initial db state

--- server/src/migrations/v2/1682386451273-Initial.ts
+++ server/src/migrations/v2/1682386451273-Initial.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class Initial1682385786665 implements MigrationInterface {
-    name = 'Initial1682385786665'
+export class Initial1682386451273 implements MigrationInterface {
+    name = 'Initial1682386451273'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "role" text DEFAULT 'USER', "profilePic" character varying, "githubId" text, "googleId" text, "username" text NOT NULL, "email" text, "password" text, CONSTRAINT "UQ_0d84cc6a830f0e4ebbfcd6381dd" UNIQUE ("githubId"), CONSTRAINT "UQ_470355432cc67b2c470c30bef7c" UNIQUE ("googleId"), CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "UQ_e12875dfb3b1d92d7d7c5377e22" UNIQUE ("email"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
