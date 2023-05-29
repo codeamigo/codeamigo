b30ad57f016567a320debe8a4811590a2ef92993
plondon <philip.n.london@gmail.com>
2023-05-29 18:15:07 -0400

    Handle openai error

--- server/src/utils/openai/index.ts
+++ server/src/utils/openai/index.ts
@@ -51,23 +51,41 @@ export const explain = async (
   prompt: string,
   apiKey: string
 ): Promise<Completion> => {
-  const configuration = new Configuration({
-    apiKey: apiKey || process.env.OPENAI_API_KEY_SECRET,
-  });
-  const openai = new OpenAIApi(configuration);
+  try {
+    const configuration = new Configuration({
+      apiKey: apiKey || process.env.OPENAI_API_KEY_SECRET,
+    });
+    const openai = new OpenAIApi(configuration);
 
-  const response = await openai.createCompletion({
-    frequency_penalty: 0,
-    max_tokens: 100,
-    model: 'text-davinci-003',
-    n: 1,
-    presence_penalty: 0,
-    prompt,
-    // eslint-disable-next-line
-    stop: ["\"\"\""],
-    temperature: 0,
-    top_p: 1,
-  });
+    const response = await openai.createCompletion({
+      frequency_penalty: 0,
+      max_tokens: 100,
+      model: 'text-davinci-003',
+      n: 1,
+      presence_penalty: 0,
+      prompt,
+      // eslint-disable-next-line
+      stop: ["\"\"\""],
+      temperature: 0,
+      top_p: 1,
+    });
 
-  return response;
+    return response;
+  } catch (e) {
+    console.log('ERROR', e);
+    return {
+      data: {
+        choices: [
+          // @ts-ignore
+          {
+            text:
+              'Sorry, we are having trouble connecting to OpenAI. Please try again later.',
+          },
+        ],
+      },
+      usage: {
+        total_tokens: 0,
+      },
+    };
+  }
 };
